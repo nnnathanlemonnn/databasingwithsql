cae060f4a602260793999894a7421bce714497fc
nnnathanlemonnn <nthirza@gmail.com>
2021-07-25 09:47:54 +0800

    first commit

---
+++ negara.sql
@@ -0,0 +1,32 @@
+INSERT INTO negara
+
+VALUES(
+	'nl',			/*KODE NEGARA*/
+	'Nathland',		/*NAMA NEGARA*/
+	'Nathland', 		/*IBUKOTA */
+	'3851555',		/*LUAS*/
+	'Krone (NOK)',		/*MATA UANG*/
+	'',			/*KODE BENUA*/
+	'59.9138',		/*LINTANG*/
+	'10.7522',		/*BUJUR*/
+	'1905-06-07'		/*BERDIRI*/
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
