fba4743b69d3a3efa9f124a42db1292c796c508e
nnnathanlemonnn <nthirza@gmail.com>
2021-07-25 10:50:43 +0800

    modified ujian.sql

--- ujian.sql
+++ ujian.sql
@@ -6,16 +6,16 @@ INSERT INTO olimpiade_matematika
 VALUES('2', 'Maharani', 'Kalimantan', '93.33');
 
 INSERT INTO olimpiade_matematika
-VALUES('3', 'Joshua', 'nusa tenggara barat', 92.39);
+VALUES('3', 'Joshua', 'nusa tenggara barat', '92.39');
 
 INSERT INTO olimpiade_matematika
-VALUES('4', 'Novianti', 'Jakarta', 85.5);
+VALUES('4', 'Novianti', 'Jakarta', '85.5');
 
 INSERT INTO olimpiade_matematika
-VALUES('5', 'Pratama', 'Bali', 95.8);
+VALUES('5', 'Pratama', 'Bali', '95.8');
 
 INSERT INTO olimpiade_matematika
-VALUES('6', 'Xevier', 'Jawa Barat', 97.3 )
+VALUES('6', 'Xevier', 'Jawa Barat', '97.3' )
 
 INSERT INTO olimpiade_matematika
 VALUES('7', 'Asri', 'Jawa Timur', '89.99');
