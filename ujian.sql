b24b30dc6042753868bd1ce641f4bf5a65b89c57
nnnathanlemonnn <nthirza@gmail.com>
2021-07-25 10:38:16 +0800

    create ujian.sql

---
+++ ujian.sql
@@ -0,0 +1,58 @@
+
+/*
+ - nomor peserta
+ - nama peserta
+ - asal provinsi
+ - nilai akhir
+ */
+
+INSERT INTO olipiade_matematika
+VALUES('1', 'Dian', 'Aceh', '89.36');
+
+INSERT INTO olimpiade_matematika
+VALUES('2', 'Maharani', 'Kalimantan', '93.33');
+
+INSERT INTO olipiade_matematika
+VALUES('3', 'Joshua', 'nusa tenggara barat', 92.39);
+
+INSERT INTO olipiade_matematika
+VALUES('4', 'Novianti', 'Jakarta', 85.5);
+
+INSERT INTO olipiade_matematika
+VALUES('5', 'Pratama', 'Bali', 95.8);
+
+INSERT INTO olipiade_matematika
+VALUES('6', 'Xevier', 'Jawa Barat', 97.3 )
+
+INSERT INTO olipiade_matematika
+VALUES('7', 'Asri', 'Jawa Timur', '89.99');
+
+INSERT INTO olipiade_matematika
+VALUES('8', 'Wati', 'Jawa Tengah', '97.3');
+
+INSERT INTO olipiade_matematika
+VALUES('9', 'Thomas', 'Maluku', '95.5');
+
+INSERT INTO olipiade_matematika
+VALUES('10', 'kenshi', 'Sumatra Utara', '92.00');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
